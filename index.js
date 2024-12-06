3115f48473170ce5f85e00175006b2eddcd72962
Edith Gatwiri <edithgatwiri70@gmail.com>
2024-12-06 09:28:03 +0300

    loop

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    //  empty array 
+    let messages = [];
+    for (let i = 0; i < namesArray.length; i++) {
+       
+        let message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+        
+        // Add the message to the messages array
+        messages.push(message);
+    }
+
+    
+    return messages;
+}
+
+const names = ["Alice", "Bob", "Edith"];
+const eventName = "birthday";
+const thankYouCards = writeCards(names, eventName);
+
+console.log(thankYouCards);
+
+
+
+function countDown(y) {
+    while (y >= 0) {
+        console.log(y);
+        y--; 
+    }
+}
+
+countDown(10);
+
+
+
